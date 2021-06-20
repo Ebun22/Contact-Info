d08876a7db6dda2536db844d27946881ad7059cd
ebun22 <subeydavd22@gmail.com>
2021-06-20 01:00:42 +0100

    Add existing file

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import ContactCard from './contactCard';
 
 class Contact extends Component{
   constructor(props){
@@ -21,28 +22,11 @@ class Contact extends Component{
 
   render(){
     const { contacts } = this.state
-    var showEmail = false
-    console.log(contacts.cell)
     return (
     <div>
       {
         contacts.length ? contacts.map(user => {
-        return(
-          <div key={user.cell}>
-            <div className="img-cont">
-              <img src={user.picture.medium} alt="some man" />
-            </div>
-            <div className="details">
-              <h4>Name: {user.name.title + " " + user.name.first + " " + user.name.last}</h4>
-              <p>Age: {user.dob.age}</p>
-              <p>Username: {user.login.username}</p>
-              <button onClick={(e,index)=>{
-                  this.showEmail = true;
-              }}>click to see email</button>
-              {showEmail ? <p>email:{user.email}</p> : null}
-            </div>
-          </div>
-        )
+        return <ContactCard user={user} key={user.cell} />
       }) :
         <div>LOADING...</div>
       }
---
+++ src/components/contactCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+ class ContactCard extends Component {
+   constructor(props){
+     super(props)
+     this.state = {
+       showEmail: false
+     }
+   }
+ render(){
+   const { user } = this.props
+   const { showEmail } = this.state
+  return (
+    <div class="md:container md:mx-auto">
+      <div className="img-cont">
+        <img src={user.picture.medium} alt="some man" />
+      </div>
+      <div className="details" class="float-right container mx-auto px-4">
+        <h4>Name: {user.name.title + " " + user.name.first + " " + user.name.last}</h4>
+        <p>Age: {user.dob.age}</p>
+        <p>Username: {user.login.username}</p>
+        <button onClick={()=>{
+            this.setState((prevState)=>({
+              showEmail: !prevState.showEmail
+            }))
+        }}>{showEmail ? "hide email" : "click to see email"}</button>
+        {showEmail ? <p>email:{user.email}</p> : null}
+      </div>
+    </div>
+  )}
+}
+
+
+export default ContactCard
