fb43fd49a4323a0e6e116a40024859a9cb7aced0
Ebun22 <ebundavid7@gmail.com>
2021-06-21 11:15:55 +0100

    Adding Updates

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -11,15 +11,16 @@ import React, { Component } from 'react'
    const { user } = this.props
    const { showEmail } = this.state
   return (
-    <div class="md:container md:mx-auto">
-      <div className="img-cont">
-        <img src={user.picture.medium} alt="some man" />
+    <<div className="cont" class= "md:p-10 md:m-5 bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+      <div className="img-cont" class="inline-block" >
+        <img src={user.picture.large} alt="some man"  class="object-contain h-48 w-full" />
       </div>
-      <div className="details" class="float-right container mx-auto px-4">
+      <div className="details" class="absolute inline-block md:p-5 text-lg ">
         <h4>Name: {user.name.title + " " + user.name.first + " " + user.name.last}</h4>
         <p>Age: {user.dob.age}</p>
         <p>Username: {user.login.username}</p>
-        <button onClick={()=>{
+        <button class="focus:outline-none bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={()=>{
             this.setState((prevState)=>({
               showEmail: !prevState.showEmail
             }))
