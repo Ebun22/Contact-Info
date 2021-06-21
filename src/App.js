97d9c2868d1911f6d107e6a1fdc53305b8496a92
Ebun22 <ebundavid7@gmail.com>
2021-06-21 11:16:55 +0100

    Adding Updates

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Contact from './components/Contacts';
  class App extends Component {
  render(){
   return (
-    <div className="App"  class="p-6 items-center justify-center">
-    <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
-        <p class="tracking-widest">This is my first React App.</p>
-      <Contact />
+   <div className="App"  class="p-6 items-center justify-center">
+    <h1 class="text-blue-400 font-extrabold text-center text-4xl md:pb-2">Hello World!</h1>
+        <p class="tracking-widest text-center text-xl md:pb-6">This is my first React App.</p>
+      <Contact class="md:container md:mx-auto" />
     </div>
   )}
 }
